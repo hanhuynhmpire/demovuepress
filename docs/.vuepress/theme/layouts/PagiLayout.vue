4b505a5a5c9a26c6f66c638cd1a618a4e48d6b74
hanhuynh <hanhuynh@mpire.vn>
2020-03-30 11:33:36 +0700

    + update code

--- docs/.vuepress/theme/layouts/PagiLayout.vue
+++ docs/.vuepress/theme/layouts/PagiLayout.vue
@@ -50,7 +50,7 @@
                                             :class="{ 'can-hide': $site.themeConfig.logo }"
                                     >{{ $siteTitle }}</span></h1>
                                     <div v-if="level == 3" class="item-date">Updated date: {{ $site.updatedDate }}</div>
-                                    <!--<PaginationPage :level="level" :ads="adsList" :pagination="paginationPage" />-->
+                                    <PaginationPage :level="level" :ads="adsList" :paginationData="paginationPage" />
                                 </div>
                             </template>
                             <template #bottom>
